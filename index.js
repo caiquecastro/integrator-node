47952fd76db8ef81708a1d5bd1c7b7f59318b646
Caique de Castro <caiquecastro@outlook.com>
2018-10-21 13:52:52 -0300

    Allow use as cli and a class

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const argv = require('./argv');
 const Integrator = require('./src/Integrator');
-const SettingsParser = require('./src/SettingsParser');
 
-const integrator = new Integrator(SettingsParser.parse(argv.settings));
-
-integrator.run()
-  .then(() => {
-    console.log('Integration finished');
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error('Integration failed', err);
-    process.exit(1);
-  });
+module.exports = Integrator;
