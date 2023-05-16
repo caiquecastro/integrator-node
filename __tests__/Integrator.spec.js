cba223769bf599ac6dff7b09839fa34b61ce8cff
Caique de Castro <caique.castro@venturus.org.br>
2023-05-16 06:06:07 -0300

    test: use mocha instead of ava

--- __tests__/Integrator.spec.js
+++ __tests__/Integrator.spec.js
@@ -1,45 +1,47 @@
-const test = require('ava');
+const { t } = require('./helpers');
 const Integrator = require('../src/Integrator');
 
-test('It requires source and destination', async (t) => {
-  try {
-    const integrator = new Integrator();
-    await integrator.run();
+describe('Integrator', () => {
+  it('It requires source and destination', async () => {
+    try {
+      const integrator = new Integrator();
+      await integrator.run();
 
-    t.fail();
-  } catch (err) {
-    t.is(err.message, 'It is required to provide source and destination config');
-  }
-});
+      t.fail();
+    } catch (err) {
+      t.is(err.message, 'It is required to provide source and destination config');
+    }
+  });
 
-test('It requires source and destination types', async (t) => {
-  try {
-    const integrator = new Integrator({
-      source: {},
-      destination: {},
-    });
-    await integrator.run();
+  it('It requires source and destination types', async () => {
+    try {
+      const integrator = new Integrator({
+        source: {},
+        destination: {},
+      });
+      await integrator.run();
 
-    t.fail();
-  } catch (err) {
-    t.is(err.message, 'It is required to provide source and destination types');
-  }
-});
+      t.fail();
+    } catch (err) {
+      t.is(err.message, 'It is required to provide source and destination types');
+    }
+  });
 
-test('It requires valid type for source and destination', async (t) => {
-  try {
-    const integrator = new Integrator({
-      source: {
-        type: 'invalid',
-      },
-      destination: {
-        type: 'invalid',
-      },
-    });
-    await integrator.run();
+  it('It requires valid type for source and destination', async () => {
+    try {
+      const integrator = new Integrator({
+        source: {
+          type: 'invalid',
+        },
+        destination: {
+          type: 'invalid',
+        },
+      });
+      await integrator.run();
 
-    t.fail();
-  } catch (err) {
-    t.is(err.message, 'Invalid type: invalid');
-  }
+      t.fail();
+    } catch (err) {
+      t.is(err.message, 'Invalid type: invalid');
+    }
+  });
 });
